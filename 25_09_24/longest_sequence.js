e728d98bc2c549c11840250027abaa568ae06b8c
Aswin060104 <trajaaswin@gmail.com>
2024-09-25 12:23:35 +0530

    added user input

--- 25_09_24/longest_sequence.js
+++ 25_09_24/longest_sequence.js
@@ -1,19 +1,38 @@
-var l = [45, 50, 35, 0, 39, 110];
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var l;
 var max = 0;
 var count = 1;
 var start = 0;
 var end = 0;
-for (var index = 0; index < l.length; index++) {
-    if (l[index] < l[index + 1])
-        count++;
-    else {
-        if (count > max) {
-            max = count;
-            start = index + 1 - count;
-            end = index + 1;
+var arraySize;
+var readline = require("readline");
+var r = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+r.question("Enter array size : ", function (size) {
+    arraySize = Number(size);
+    r.question("Enter array elements : ", function (elements) {
+        var stringArray = elements.split(" ");
+        var numberArray = [];
+        for (var _i = 0, stringArray_1 = stringArray; _i < stringArray_1.length; _i++) {
+            var tem = stringArray_1[_i];
+            numberArray.push(Number(tem));
         }
-        count = 1;
-    }
-}
-console.log("The Maximum value is : ", max);
-console.log(l.slice(start, end));
+        for (var index = 0; index < numberArray.length; index++) {
+            if (numberArray[index] < numberArray[index + 1])
+                count++;
+            else {
+                if (count > max) {
+                    max = count;
+                    start = index + 1 - count;
+                    end = index + 1;
+                }
+                count = 1;
+            }
+        }
+        console.log("The Maximum value is : ", max);
+        console.log(numberArray.slice(start, end));
+    });
+});
--- 25_09_24/longest_sequence.ts
+++ 25_09_24/longest_sequence.ts
@@ -1,24 +1,43 @@
-var l : number[] = [45, 50, 35, 0 ,39, 110];
+var l : number[];
 var max : number = 0;
 var count : number = 1;
 var start : number = 0;
 var end : number = 0;
+var arraySize : number;
 
+import * as readline from 'readline';
 
-for(var index = 0; index < l.length; index++){
+const r = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+});
+
+r.question("Enter array size : ", (size : string) => {
+    arraySize = Number(size);
+    r.question("Enter array elements : ", (elements : string) => {
+        
+        var stringArray : string[] = elements.split(" ");
+        var numberArray : number[] = [];
+
+        for(var tem of stringArray)
+            numberArray.push(Number(tem));
+
+        for(var index = 0; index < numberArray.length; index++){
     
-    if( l[index] < l[index +1 ])
-        count++;
-    else{
-        if(count > max){
-            max = count;
-            start = index + 1 - count;
-            end = index + 1
+            if( numberArray[index] < numberArray[index +1 ])
+                count++;
+            else{
+                if(count > max){
+                    max = count;
+                    start = index + 1 - count;
+                    end = index + 1
+                }
+                count = 1;
+            }
         }
-        count = 1;
-    }
-}
-
-console.log("The Maximum value is : " ,max);
-
-console.log(l.slice(start,end));
+        
+        console.log("The Maximum value is : " ,max);
+        
+        console.log(numberArray.slice(start,end));
+   });
+});
