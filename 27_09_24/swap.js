f86f5f80d52e5ddd42917c501cea41d6f0f1eeaf
Aswin060104 <trajaaswin@gmail.com>
2024-09-27 16:43:44 +0530

    swap without tem variable

---
+++ 27_09_24/swap.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var readline = require("readline");
+var c = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+c.question("Enter a and b : ", function (values) {
+    console.log(values.split(" "));
+    var a = Number((values.split(" "))[0]);
+    var b = Number((values.split(" "))[1]);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    console.log("Values of a and b are ", a, b);
+});
