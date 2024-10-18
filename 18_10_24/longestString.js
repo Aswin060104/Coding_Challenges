68f83f8aa93d112a6562039092c11f9ea831b203
Aswin060104 <trajaaswin@gmail.com>
2024-10-18 18:08:11 +0530

    longest substring

--- 18_10_24/longestString.js
+++ 18_10_24/longestString.js
@@ -1,21 +1,29 @@
 var s1 = "javascript express";
 var s2 = "rtscript ety";
-var ans = "";
-for (var i = 0; i < s1.length; i++) {
-    var tem = "";
-    for (var j = 0; j < s2.length; j++) {
+var ans = 0;
+var ansString = "";
+var i = 0;
+while (i < s1.length) {
+    var j = 0; // Reset j for each iteration of i
+    while (j < s2.length) {
         if (s1[i] == s2[j]) {
             var x = i;
             var y = j;
-            while (s1[x] == s2[y]) {
+            var tem = "";
+            // Continue while characters from s1 and s2 are matching
+            while (x < s1.length && y < s2.length && s1[x] == s2[y]) {
                 tem += s1[x];
                 x++;
                 y++;
             }
-            if (tem.length > ans.length)
-                ans = tem;
-            tem = "";
+            if (tem.length > ans) {
+                ansString = tem;
+                ans = tem.length;
+            }
         }
+        j++;
     }
+    i++;
 }
-console.log(ans);
+console.log("Longest common substring:", ansString);
+console.log("Length of longest common substring:", ans);
--- 18_10_24/longestString.ts
+++ 18_10_24/longestString.ts
@@ -1,21 +1,32 @@
-var s1 : string = "javascript express";
-var s2 : string = "rtscript ety"
-var ans : string = "";
-for(var i : number = 0; i < s1.length; i++){
-    var tem : string = "";
-    for(var j : number = 0; j < s2.length; j++){
-        if(s1[i] == s2[j]){
+var s1: string = "javascript express";
+var s2: string = "rtscript ety";
+var ans = 0;
+var ansString = "";
+var i = 0;
+
+while (i < s1.length) {
+    var j = 0; // Reset j for each iteration of i
+    while (j < s2.length) {
+        if (s1[i] == s2[j]) {
             var x = i;
             var y = j;
-            while(s1[x] == s2[y]){
+            var tem = "";
+            // Continue while characters from s1 and s2 are matching
+            while (x < s1.length && y < s2.length && s1[x] == s2[y]) {
                 tem += s1[x];
                 x++;
                 y++;
             }
-            if( tem.length > ans.length)
-                ans = tem;
-            tem = "";
-        }  
+            if (tem.length > ans) {
+                ansString = tem;
+                ans = tem.length;
+            }
+        }
+        j++;
     }
+    i++;
 }
-console.log(ans);
+
+console.log("Longest common substring:", ansString);
+console.log("Length of longest common substring:", ans);
+
