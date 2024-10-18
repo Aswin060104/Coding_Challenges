e2092cf17f66d43f64e7622b8c7ced6a48e84f57
Aswin060104 <trajaaswin@gmail.com>
2024-10-18 16:09:50 +0530

    longest commom substring

---
+++ 18_10_24/longestString.js
@@ -0,0 +1,21 @@
+var s1 = "javascript express";
+var s2 = "rtscript ety";
+var ans = "";
+for (var i = 0; i < s1.length; i++) {
+    var tem = "";
+    for (var j = 0; j < s2.length; j++) {
+        if (s1[i] == s2[j]) {
+            var x = i;
+            var y = j;
+            while (s1[x] == s2[y]) {
+                tem += s1[x];
+                x++;
+                y++;
+            }
+            if (tem.length > ans.length)
+                ans = tem;
+            tem = "";
+        }
+    }
+}
+console.log(ans);
